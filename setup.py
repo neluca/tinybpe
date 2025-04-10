b0e741c4be55a01919b25aa83e827b37410a8e6a
liao yinan <myneluca@gmail.com>
2025-04-11 04:46:28 +0800

    add tinybpe packaging config files

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 import sys
-import os
-import shutil
 from setuptools import Extension, setup
 
 ext_modules = [
     Extension(
-        "bpe",
+        "tinybpe.bpe",
         sources=[
             "src/bpe_module.c",
             "src/_tree_core.c",
@@ -19,20 +17,6 @@ ext_modules = [
 
 setup(
     name="tinybpe",
-    version="1.0.0",
+    packages=["tinybpe"],
     ext_modules=ext_modules
 )
-
-bpe_file_path = None
-
-for root, dirs, files in os.walk("build"):
-    for file in files:
-        if (file.endswith(".pyd") or file.endswith(".so") or file.endswith(".dylib")) \
-                and file.startswith("bpe"):
-            bpe_file_path = os.path.join(root, file)
-            break
-
-if bpe_file_path is not None:
-    shutil.copy(bpe_file_path, "tinybpe")
-else:
-    raise RuntimeError("Error ...")
