610244a12fdf52580d233515c2067cfd999f616c
liao yinan <myneluca@gmail.com>
2025-04-12 04:15:19 +0800

    add loading code of remaps

--- scripts/tools.py
+++ scripts/tools.py
@@ -14,4 +14,15 @@ def bpe_save_remaps(file_prefix: str, remaps: list[int]):
     with open(remaps_file, 'w') as f:
         f.write("tinybpe remaps\n")
         for i in remaps:
-            f.write(f"{i}")
+            f.write(f"{i}\n")
+
+
+def bpe_load_remaps(remaps_file: str) -> list[int]:
+    assert remaps_file.endswith(".map")
+    remaps = []
+    with open(remaps_file, 'r', encoding="utf-8") as f:
+        magic = f.readline().strip()
+        assert magic == "tinybpe remaps"
+        for line in f:
+            remaps.append(int(line))
+    return remaps
