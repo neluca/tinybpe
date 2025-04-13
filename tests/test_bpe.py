9db96005dcf54efaf64b7760912eafffed4870a4
liao yinan <myneluca@gmail.com>
2025-04-14 03:50:42 +0800

    add test core bpe

--- tests/test_bpe.py
+++ tests/test_bpe.py
@@ -1,4 +1,27 @@
 import unittest
-from tinybpe import bpe
+from tinybpe import bpe, Tokenizer
 
 
+class TestBPETokenizer(unittest.TestCase):
+    def test_encode(self):
+        pass
+
+
+class TestBPETrainer(unittest.TestCase):
+    def test_train(self):
+        trainer = bpe.Trainer([b"Hello TinyBPE", b"1234567890"])
+
+
+class TestBPEBytesRemap(unittest.TestCase):
+    def test_bytes_rmap(self):
+        pass
+
+
+class TestTokenizer(unittest.TestCase):
+    def test_encode(self):
+        pass
+
+
+class TestSimpleTrainer(unittest.TestCase):
+    def test_train(self):
+        pass
