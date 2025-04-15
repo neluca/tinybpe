03b54816b3d93cc06b6c40ac924028642429cf75
liao yinan <myneluca@gmail.com>
2025-04-16 03:54:15 +0800

    delete and change

--- scripts/README.md
+++
@@ -1,7 +0,0 @@
-llama: meta-llama/Llama-4-Scout-17B-16E-Instruct
-
-qwen: Qwen2.5-7B-Instruct
-
-yi: Yi-1.5-6B-Chat
-
-gpt4: 
--- scripts/convert_llama.py
+++
@@ -1,5 +0,0 @@
-from tools import bpe_load_remaps
-
-maps = bpe_load_remaps("cl100k_base.map")
-print(len(maps))
-print(maps)
