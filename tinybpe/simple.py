c8b4ce66a2a494c634e0c8bd03d83426dec51dd2
liao yinan <myneluca@gmail.com>
2025-04-16 04:28:46 +0800

    optimize the code structure.

--- tinybpe/simple.py
+++ tinybpe/simple.py
@@ -1,9 +1,10 @@
-from ._utils import _save_bpe_merges
+from ._utils import save_bpe_model
 from . import bpe
 from typing import Callable, Optional
 
 
 class Trainer(bpe.Trainer):
+    """ A simple Byte-Pair-Encoding trainer """
     def __init__(self, text: str, preprocess: Optional[Callable[[str], list[bytes | bytearray]]] = None):
         if preprocess is None:
             text_bytes_list = [text.encode("utf-8")]
@@ -13,4 +14,4 @@ class Trainer(bpe.Trainer):
         super().__init__(text_bytes_list)
 
     def save(self, file_prefix: str) -> None:
-        _save_bpe_merges(file_prefix, self.merges)
+        save_bpe_model(file_prefix, self.merges)
