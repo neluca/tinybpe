f45c9c13ce92cc2106fd0a36e3b4c095c1711637
liao yinan <myneluca@gmail.com>
2025-04-16 03:54:15 +0800

    delete and change

--- scripts/convert_gpt4.py
+++
@@ -1,7 +0,0 @@
-import tiktoken
-from tools_tiktoken import bpe_mergeable_ranks_save
-
-enc = tiktoken.get_encoding("cl100k_base")
-
-bpe_mergeable_ranks_save("cl100k_base", enc._mergeable_ranks)
-print(enc._special_tokens)
