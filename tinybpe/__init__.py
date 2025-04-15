6d13e2b876b495e9dd47a5657d279b3303c2cf9f
liao yinan <myneluca@gmail.com>
2025-04-16 04:28:46 +0800

    optimize the code structure.

--- tinybpe/__init__.py
+++ tinybpe/__init__.py
@@ -1,56 +1,5 @@
 __version__ = "0.1.0"
 
-from . import bpe
+from ._utils import load_bpe_model, save_bpe_model, save_from_tiktoken, get_from_tiktoken
+from .core import CommonTokenizer, Tokenizer
 from .simple import Trainer as SimpleTrainer
-from ._abc import ABCTokenizer
-from ._utils import load_bpe_file
-from typing import Callable, Optional
-
-
-class Tokenizer(ABCTokenizer):
-    def __init__(self, merges: list[tuple[int, int]], special_tokens: Optional[dict[str, int]] = None,
-                 pre_tokenize: Optional[Callable[[str], list[bytes]]] = None):
-        if special_tokens is None:
-            self._enc = bpe.Tokenizer(merges)
-        else:
-            _special_tokens = {k.encode("utf-8"): v for k, v in special_tokens.items()}
-            self._enc = bpe.Tokenizer(merges, _special_tokens)
-
-        if pre_tokenize is None:
-            self._tokenize = lambda s: [s.encode("utf-8")]
-        else:
-            self._tokenize = pre_tokenize
-
-    def encode(self, text: str) -> list[int]:
-        text_bytes_list = self._tokenize(text)
-        ids = sum(list(map(self._enc.encode, text_bytes_list)), [])
-        return ids
-
-    def decode(self, ids: list[int]) -> str:
-        text_bytes = self._enc.decode(ids)
-        return text_bytes.decode("utf-8")
-
-    def stream_decode(self, callback_fn: Callable[[str], None]) -> Callable[[int], None]:
-        self._enc.cache_clean()
-
-        def _decode(token_id: int):
-            text_bytes = self._enc.cache_decode(token_id)
-            if text_bytes is not None:
-                callback_fn(text_bytes.decode("utf-8"))
-
-        return _decode
-
-    def stream_decode_cache_clean(self):
-        self._enc.cache_clean()
-
-    @property
-    def merges(self) -> list[tuple[int, int]]:
-        return self._enc.merges
-
-    @property
-    def vocab(self) -> dict[int, bytes]:
-        return self._enc.vocab
-
-    @property
-    def n_vocab(self) -> int:
-        return self._enc.size
